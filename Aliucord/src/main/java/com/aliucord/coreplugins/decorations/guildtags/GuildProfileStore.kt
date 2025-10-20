8a9213e64f08efcb7d3371e0778af575bf5a7e9c
Cilly Leang <me@lava.moe>
2025-10-20 23:11:30 +1100

    fix: fix build error
    a

--- Aliucord/src/main/java/com/aliucord/coreplugins/decorations/guildtags/GuildProfileStore.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/decorations/guildtags/GuildProfileStore.kt
@@ -9,7 +9,7 @@ import com.discord.api.commands.Application
 
 private val logger = Logger("GuildProfileStore")
 
-internal data class GuildProfile(
+data class GuildProfile(
     val id: Long,
     val name: String,
     val iconHash: String?,
